c0298b376b631a0927344643baa3d37a2d1d1d95
Andrew Nelson <andy@andyhub.com>
2015-09-04 22:00:02 -0400

    set direction of pin 16

--- lightMonitor.js
+++ lightMonitor.js
@@ -1,7 +1,9 @@
 var gpio = require("pi-gpio");
 
-// To read pin 16
-gpio.read(25, function(err, value) {
-    if(err) throw err;
-    console.log(value); // The current state of the pin
-});
+gpio.setDirection(16,input, function() {
+    // To read pin 16
+    gpio.read(16, function(err, value) {
+        if(err) throw err;
+        console.log(value); // The current state of the pin
+    });
+}
